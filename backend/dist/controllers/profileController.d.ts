f29e0a412ac0d750714d615f3931e88a34c069da
Ashik Muhammed T <ashikthanzeer6@outlook.com>
2026-06-07 23:11:42 +0530

    Delete backend/dist directory

--- backend/dist/controllers/profileController.d.ts
+++
@@ -1,8 +0,0 @@
-import { Response } from 'express';
-import { AuthenticatedRequest } from '../middleware/errorHandler';
-export declare function getProfile(req: AuthenticatedRequest, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-export declare function updateProfile(req: AuthenticatedRequest, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-export declare function getPreferences(req: AuthenticatedRequest, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-export declare function updatePreferences(req: AuthenticatedRequest, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-export declare function getGamification(req: AuthenticatedRequest, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=profileController.d.ts.map
--- backend/dist/utils/auth.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function hashPassword(password: string): Promise<string>;
-export declare function comparePasswords(password: string, hash: string): Promise<boolean>;
-export declare function generateToken(id: number, email: string): string;
-export declare function isValidEmail(email: string): boolean;
-//# sourceMappingURL=auth.d.ts.map
